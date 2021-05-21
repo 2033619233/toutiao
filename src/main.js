b52379db9c37fccb50d6302bfd5af4929e28f39d
“chengqingyuan git config --global user.name “chengqingyuan <2033619233@qq.com>
2021-05-21 10:58:14 +0800

    '第一次提交'

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
 
 createApp(App).use(store).use(router).mount('#app')
